561a1f6fbbb6002ae9d0b43174e92106c85edcf0
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-10-07 08:35:51 +0200

    Update sd-webui-predefined_aspect_ratios.js

--- javascript/sd-webui-predefined_aspect_ratios.js
+++ javascript/sd-webui-predefined_aspect_ratios.js
@@ -1,19 +1,20 @@
 if ("undefined" === typeof ar_button_titles) {
-    aspect_ratio_button_titles = {};
+	ar_button_titles = {};
 }
 
-aspect_ratio_button_titles["Calc"] = "Show or hide the aspect ratio calculator";
-aspect_ratio_button_titles["\u{21c5}"] = "Swap width and height values";
-aspect_ratio_button_titles["\u2B07\ufe0f"] = "Get dimensions from txt2img/img2img sliders";
-aspect_ratio_button_titles["\u{1f5bc}"] = "Get dimensions from image on current img2img tab";
-aspect_ratio_button_titles["Calculate Height"] = "Calculate new height based on source aspect ratio";
-aspect_ratio_button_titles["Calculate Width"] = "Calculate new width based on source aspect ratio";
-aspect_ratio_button_titles["Apply"] = "Apply calculated width and height to txt2img/img2img sliders";
+ar_button_titles["Calc"] = "Show or hide the aspect ratio calculator";
+ar_button_titles["\u{21c5}"] = "Swap width and height values";
+ar_button_titles["\u2B07\ufe0f"] = "Get dimensions from txt2img/img2img sliders";
+ar_button_titles["\u{1f5bc}"] = "Get dimensions from image on current img2img tab";
+ar_button_titles["Calculate Height"] = "Calculate new height based on source aspect ratio";
+ar_button_titles["Calculate Width"] = "Calculate new width based on source aspect ratio";
+ar_button_titles["Apply"] = "Apply calculated width and height to txt2img/img2img sliders";
 
 onUiUpdate(function(){
-    gradioApp().querySelectorAll('txt2img_container_aspect_ratio button, img2img_container_aspect_ratio button').forEach(function(elem){
-	tooltip = aspect_ratio_button_titles[elem.textContent];
-	    if(tooltip){
-		elem.title = tooltip;
-            }
-    })
+	gradioApp().querySelectorAll('#txt2img_container_aspect_ratio button, #img2img_container_aspect_ratio button').forEach(function(elem){
+		tooltip = ar_button_titles[elem.textContent];
+		if(tooltip){
+		 	elem.title = tooltip;
+		}
+	})
+})
