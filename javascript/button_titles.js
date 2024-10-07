65bcf265a924bb82e3c8e84993f806087c845da1
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-10-07 07:47:52 +0200

    Update button_titles.js

--- javascript/button_titles.js
+++ javascript/button_titles.js
@@ -1,2 +1,2 @@
-// Do not put custom titles here. This file is overwritten each time the web UI is started.
+// Do not put custom titles here. This file will be overwritten each time the web UI is started.
 aspect_ratio_button_titles = {}
