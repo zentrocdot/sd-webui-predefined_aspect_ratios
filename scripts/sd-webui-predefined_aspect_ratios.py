e5c55c7c48a5393a47c367af1cffc74fa001cd43
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-10-07 11:27:04 +0200

    Update sd-webui-predefined_aspect_ratios.py

--- scripts/sd-webui-predefined_aspect_ratios.py
+++ scripts/sd-webui-predefined_aspect_ratios.py
@@ -1,7 +1,6 @@
 import contextlib
 import gradio as gr
 import modules.scripts as scripts
-#from modules.scripts import scripts
 #from pathlib import Path
 from modules.ui_components import ToolButton
 #from math import gcd
