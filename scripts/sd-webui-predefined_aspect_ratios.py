ce6c26afc13277e70aa8a5c603f1b36202428c00
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-10-07 10:34:29 +0200

    Update sd-webui-predefined_aspect_ratios.py

--- scripts/sd-webui-predefined_aspect_ratios.py
+++ scripts/sd-webui-predefined_aspect_ratios.py
@@ -5,11 +5,11 @@ from pathlib import Path
 from modules.ui_components import ToolButton
 from math import gcd
 
-aspect_ratios_dir = scripts.basedir()
+#aspect_ratios_dir = scripts.basedir()
 
-switch_values_symbol = "\U000021C5"
-get_dimensions_symbol = "\u2B07\ufe0f"
-get_image_dimensions_symbol = "\U0001F5BC"
+#switch_values_symbol = "\U000021C5"
+#get_dimensions_symbol = "\u2B07\ufe0f"
+#get_image_dimensions_symbol = "\U0001F5BC"
 
 class ARButton(ToolButton):
     def __init__(self, ar=1.0, **kwargs):
@@ -29,23 +29,6 @@ class ARButton(ToolButton):
     def reset(self, w, h):
         return [self.res, self.res]
 
-'''def write_js_titles_file(button_titles):
-    filename = Path(aspect_ratios_dir, "javascript", "button_titles.js")
-    content = [
-        "// Do not put custom titles here. This file is overwritten each time the web UI is started.\n"
-    ]
-    content.append("ar_button_titles = {\n")
-    counter = 0
-    while counter < len(button_titles[0]):
-        content.append(
-            f'    "{button_titles[0][counter]}" : "{button_titles[1][counter]}",\n'
-        )
-        counter = counter + 1
-    content.append("}")
-    with open(filename, "w", encoding="utf-8") as f:
-        f.writelines(content)
-        '''
-
 class AspectRatioScript(scripts.Script):
     def title(self):
         return "Aspect Ratio Selector"
@@ -64,22 +47,10 @@ class AspectRatioScript(scripts.Script):
                     visible=True,
                     elem_id="arc_empty_space",
                 )
-                
-                self.aspect_ratios = [
-                   "1:1, 1.0 # 1:1 ratio based on minimum dimension",
-                   "3:2, 3/2 # Set width based on 3:2 ratio to height",
-                   "4:3, 4/3 # Set width based on 4:3 ratio to height",
-                   "16:9, 16/9 # Set width based on 16:9 ratio to height"
+                self.aspect_ratios = [1.0, 3/2, 4/3, 16/9]
                 ]
-                self.aspect_ratio_labels = [
-                   "1:1",
-                   "3:2",
-                   "4:3",
-                   "16:9",
-                ]
-                #self.res = ["512,512", "512,768"]
-                #self.res_labels = ["512,512", "512,768"]
-                # Aspect Ratio buttons
+                self.aspect_ratio_labels = ["1:1", "3:2", "4:3", "16:9"]
+                # Aspect ratio buttons.
                 btns = [
                     ARButton(ar=ar, value=label)
                     for ar, label in zip(
@@ -98,13 +69,7 @@ class AspectRatioScript(scripts.Script):
                             inputs=resolution,
                             outputs=resolution,
                         )
-            # Write button_titles.js with labels and comments read from aspect ratios and resolutions files
-            #button_titles = [self.aspect_ratio_labels + self.res_labels]
-            #self.aspect_ratio_comments = ["1", "2", "3", "4"]
-            #self.res_comments = ["1", "2", "3", "4"]
-            #button_titles.append(self.aspect_ratio_comments + self.res_comments)
-            #write_js_titles_file(button_titles)
-            # dummy components needed for JS function
+            # Dummy components needed for JS function
             dummy_text1 = gr.Text(visible=False)
             dummy_text2 = gr.Text(visible=False)
             dummy_text3 = gr.Text(visible=False)
