2aba46167dcc886ce69c6182542741b9739644da
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-10-07 11:13:05 +0200

    Update sd-webui-predefined_aspect_ratios.py

--- scripts/sd-webui-predefined_aspect_ratios.py
+++ scripts/sd-webui-predefined_aspect_ratios.py
@@ -58,12 +58,6 @@ class AspectRatioScript(scripts.Script):
                             inputs=resolution,
                             outputs=resolution
                         )
-                        print(b) 
-            # Dummy components needed for JS function
-            #dummy_text1 = gr.Text(visible=False)
-            #dummy_text2 = gr.Text(visible=False)
-            #dummy_text3 = gr.Text(visible=False)
-            #dummy_text4 = gr.Text(visible=False)
             
     # Function after_component()
     def after_component(self, component, **kwargs):
