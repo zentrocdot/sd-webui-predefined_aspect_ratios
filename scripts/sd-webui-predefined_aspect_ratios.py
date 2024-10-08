2ba15b6c38405dfcd134ad195603b1b63e63364e
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-10-08 09:18:59 +0200

    Update sd-webui-predefined_aspect_ratios.py

--- scripts/sd-webui-predefined_aspect_ratios.py
+++ scripts/sd-webui-predefined_aspect_ratios.py
@@ -8,7 +8,6 @@ Version 0.0.0.5
 # pylint: disable=attribute-defined-outside-init
 # pylint: disable=import-error
 # pylint: disable=consider-using-from-import
-# pylint: disable=trailing-whitespace
 # pylint: disable=unused-argument
 # pylint: disable=too-many-instance-attributes
 # pylint: disable=no-self-use
